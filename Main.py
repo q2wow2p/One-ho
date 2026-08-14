3e512798ed759aec8f093963a6dd90ce75ac1774
q2wow2p <q.2.w.o.w.2.p@gmail.com>
2026-08-14 20:18:29 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,8 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
